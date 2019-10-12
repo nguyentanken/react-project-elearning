5a8515d0ab09f4660f6b51be5709c17df4818f40
NguyenThien <tranthien8789@gmail.com>
2019-10-12 11:30:52 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 // import sass và bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./Sass/main.scss";
 // import provider & store
 import { Provider } from "react-redux";
